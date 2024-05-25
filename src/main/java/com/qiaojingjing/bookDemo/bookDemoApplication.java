33dfc8d31ab3ffefcad239384da5084783b68d7d
qiaojingjing <1507988959@qq.com>
2024-05-25 12:13:15 +0800

    style:更改命名

--- src/main/java/com/qiaojingjing/bookDemo/bookDemoApplication.java
+++ src/main/java/com/qiaojingjing/bookDemo/bookDemoApplication.java
@@ -1,13 +1,13 @@
-package com.qiaojingjing.demo;
+package com.qiaojingjing.bookDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class bookDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(bookDemoApplication.class, args);
     }
 
 }
--- src/test/java/com/qiaojingjing/bookDemo/DemoApplicationTests.java
+++ src/test/java/com/qiaojingjing/bookDemo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.qiaojingjing.demo;
+package com.qiaojingjing.bookDemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
