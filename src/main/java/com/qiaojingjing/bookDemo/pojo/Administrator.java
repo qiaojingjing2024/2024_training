0477d1cabe3fd7308a0c85b14b057df016fe42ca
qiaojingjing <1507988959@qq.com>
2024-05-26 15:31:42 +0800

    style:重命名

--- src/main/java/com/qiaojingjing/bookDemo/pojo/Administrator.java
+++ src/main/java/com/qiaojingjing/bookDemo/pojo/Administrator.java
@@ -1,13 +1,15 @@
 package com.qiaojingjing.bookDemo.pojo;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class administrator {
+@Builder
+public class Administrator {
     private Long id;
     private String name;
     private String username;
